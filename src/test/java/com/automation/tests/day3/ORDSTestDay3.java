2131d974b55766416964f0de9618b46cd5c12cfd
Jarylgapova <Karima.jarylgapova@gmail.com>
2020-05-26 13:15:06 -0500

    day4

--- src/test/java/com/automation/tests/day3/ORDSTestDay3.java
+++ src/test/java/com/automation/tests/day3/ORDSTestDay3.java
@@ -17,8 +17,8 @@ import static org.hamcrest.Matchers.lessThan;
 public class ORDSTestDay3 {
 
     @BeforeAll
-    public static void setup(){
-        baseURI = "http://54.224.118.38:1000/ords/hr/";
+    public static void setup() {
+        baseURI = "http://54.224.118.38:1000/ords/hr";
     }
 
     /**
@@ -29,30 +29,52 @@ public class ORDSTestDay3 {
      * and assert that region name is Europe
      * and assert that region id is 1
      */
+
     @Test
-    public void verifyFirstRegion(){
-        given().pathParam("id", 1).
-                when().get("/regions/{id}").prettyPeek().
-                then().assertThat().statusCode(200).
+    public void verifyFirstRegion() {
+        given().
+                pathParam("id", 1).
+                when().
+                get("/regions/{id}").prettyPeek().
+                then().assertThat().
+                statusCode(200).
                 body("region_name", is("Europe")).
                 body("region_id", is(1)).
-                time(lessThan(5L), TimeUnit.SECONDS); //verify that response time is less than 5 sec
+                time(lessThan(5L), TimeUnit.SECONDS); //verify that response time is less than 5 seconds
 
     }
 
     @Test
     public void verifyEmployee() {
-        Response response = given().accept(ContentType.JSON).
-                when().get("/employees");
+        Response response = given().
+                accept(ContentType.JSON).
+                when().
+                get("/employees");
 
+        /**
+         * sonPath is an alternative to using XPath for easily getting values from a Object document. It follows the
+         *  * Groovy <a href="http://docs.groovy-lang.org/latest/html/documentation/#_gpath">GPath</a>
+         *  syntax when getting an object from the document. You can regard it as an alternative to XPath for JSON.
+         */
         JsonPath jsonPath = response.jsonPath();
 
+        //items - name of the array where all employees are stored
+        //GPath, something like XPath bit different. GPath use Groovy syntax
         String nameOfFirstEmployee = jsonPath.getString("items[0].first_name");//0 - to get first item in the list
         String nameOfLastEmployee = jsonPath.getString("items[-1].first_name");//-1 - to get last item in the list, like length() - n
 
-        System.out.println("First name first employee: " + nameOfFirstEmployee);
 
-        System.out.println("First name of the last employee: " + nameOfLastEmployee );
+        Map<String, ?> searchByName = jsonPath.get("items.find{it.last_name == 'Hunold'}");
+        String searchByEmail = jsonPath.get("items.find{it.email == 'BERNST'}.last_name");
+        String maxSalary = jsonPath.get("items.max{it.salary}.last_name");
+
+        System.out.println(searchByName);
+        System.out.println(searchByEmail);
+        System.out.println(maxSalary);
+
+        System.out.println("First name of 1st employee: " + nameOfFirstEmployee);
+        System.out.println("First name of last employee: " + nameOfLastEmployee);
+
 
         Map<String, ?> firstEmployee = jsonPath.get("items[0]");
         System.out.println(firstEmployee);
